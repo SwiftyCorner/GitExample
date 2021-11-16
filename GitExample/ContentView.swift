26aff32f37785a0ed70d83b93f0d02414fde8b25
Patrick Frei <p_frei@outlook.com>
2021-11-16 03:21:25 +0100

    color purple

--- GitExample/ContentView.swift
+++ GitExample/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         NavigationView {
             NavigationLink(destination: ModalView()) {
                 Text("go next level")
-            }
+            }.foregroundColor(.purple)
             .navigationTitle("Xcode and git")
         }
         
